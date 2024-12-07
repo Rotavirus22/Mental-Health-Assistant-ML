76a9121f982e8a2d7f4908396a39c4fd65ee6cff
rotavirus22 <rohanpokhrel7@gmail.com>
2024-12-07 14:06:05 +0545

    Changed model

--- mental.py
+++ mental.py
@@ -76,7 +76,7 @@ def compute_auc(y_true, y_prob):
         tpr.append(TP / total_positives)
         fpr.append(FP / total_negatives)
 
-    auc = np.trapz(tpr, fpr)
+    auc = np.trapezoid(tpr, fpr)
     return auc
 
 
@@ -112,197 +112,21 @@ class LogisticRegressionManual:
     def _sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
-
-# KNN Classifier
-class KNeighborsClassifierManual:
-    def __init__(self, n_neighbors=1):
-        self.n_neighbors = n_neighbors
-
-    def fit(self, X, y):
-        self.X_train = X.astype(float)
-        self.y_train = y
-
-    def predict(self, X):
-        X = X.astype(float)
-        predictions = []
-        for x_test in X:
-            distances = np.linalg.norm(self.X_train - x_test, axis=1)
-            k_indices = np.argsort(distances)[:self.n_neighbors]
-            k_nearest_labels = [self.y_train[i] for i in k_indices]
-            predictions.append(np.bincount(k_nearest_labels).argmax())
-        return np.array(predictions)
-
-
-# Decision Tree Classifier
-class DecisionTreeClassifierManual:
-    def __init__(self, max_depth=5):
-        self.max_depth = max_depth
-        self.tree = None
-
-    def fit(self, X, y):
-        self.tree = self._build_tree(X, y)
-
-    def _build_tree(self, X, y, depth=0):
-        if len(set(y)) == 1:
-            return y[0]
-
-        if X.shape[1] == 0 or X.shape[0] == 0:
-            return np.bincount(y).argmax() if len(y) > 0 else 0
-
-        if self.max_depth is not None and depth >= self.max_depth:
-            return np.bincount(y).argmax() if len(y) > 0 else 0
-
-        best_feature, best_threshold = self._best_split(X, y)
-
-        if best_feature is None:
-            return np.bincount(y).argmax() if len(y) > 0 else 0
-
-        left_indices = X[:, best_feature] < best_threshold
-        right_indices = X[:, best_feature] >= best_threshold
-
-        left_tree = self._build_tree(X[left_indices], y[left_indices], depth + 1)
-        right_tree = self._build_tree(X[right_indices], y[right_indices], depth + 1)
-
-        return (best_feature, best_threshold, left_tree, right_tree)
-
-    def _best_split(self, X, y):
-        best_gain = -1
-        best_feature = None
-        best_threshold = None
-        for feature_index in range(X.shape[1]):
-            thresholds = np.unique(X[:, feature_index])
-            for threshold in thresholds:
-                gain = self._information_gain(X, y, feature_index, threshold)
-                if gain > best_gain:
-                    best_gain = gain
-                    best_feature = feature_index
-                    best_threshold = threshold
-        return best_feature, best_threshold
-
-    def _information_gain(self, X, y, feature_index, threshold):
-        parent_entropy = self._entropy(y)
-        left_indices = X[:, feature_index] < threshold
-        right_indices = X[:, feature_index] >= threshold
-
-        left_entropy = self._entropy(y[left_indices])
-        right_entropy = self._entropy(y[right_indices])
-
-        left_weight = len(y[left_indices]) / len(y)
-        right_weight = len(y[right_indices]) / len(y)
-
-        return parent_entropy - (left_weight * left_entropy + right_weight * right_entropy)
-
-    def _entropy(self, y):
-        proportions = np.bincount(y) / len(y)
-        return -np.sum(proportions * np.log2(proportions + 1e-6))
-
-    def _predict_single(self, x, tree):
-        if not isinstance(tree, tuple):
-            return tree
-        feature_index, threshold, left_tree, right_tree = tree
-        if x[feature_index] < threshold:
-            return self._predict_single(x, left_tree)
-        else:
-            return self._predict_single(x, right_tree)
-
-    def predict(self, X):
-        return np.array([self._predict_single(x, self.tree) for x in X])
-
-
-# Random Forest Classifier
-class RandomForestClassifierManual:
-    def __init__(self, n_estimators=10, max_depth=None, min_samples_split=2):
-        self.n_estimators = n_estimators
-        self.max_depth = max_depth
-        self.min_samples_split = min_samples_split
-        self.trees = []
-
-    def fit(self, X, y):
-        for _ in range(self.n_estimators):
-            tree = DecisionTreeClassifierManual(max_depth=self.max_depth)
-            bootstrap_indices = np.random.choice(len(X), len(X), replace=True)
-            X_bootstrap = X[bootstrap_indices]
-            y_bootstrap = y[bootstrap_indices]
-            tree.fit(X_bootstrap, y_bootstrap)
-            self.trees.append(tree)
-
-    def predict(self, X):
-        tree_preds = np.array([tree.predict(X) for tree in self.trees])
-        return np.array([Counter(tree_pred).most_common(1)[0][0] for tree_pred in tree_preds.T])
-
-
-# Gaussian Naive Bayes
-class GaussianNBManual:
-    def fit(self, X, y):
-        self.classes = np.unique(y)
-        self.params = {}
-        for c in self.classes:
-            X_c = X[y == c]
-            self.params[c] = {
-                'mean': np.mean(X_c, axis=0),
-                'var': np.var(X_c, axis=0),
-                'prior': len(X_c) / len(y)
-            }
-
-    def predict(self, X):
-        probs = []
-        for x in X:
-            class_probs = {}
-            for c in self.classes:
-                prior = np.log(self.params[c]['prior'])
-                likelihood = np.sum(np.log(self.gaussian_pdf(x, self.params[c]['mean'], self.params[c]['var'])))
-                class_probs[c] = prior + likelihood
-            probs.append(max(class_probs, key=class_probs.get))
-        return np.array(probs)
-
-    def gaussian_pdf(self, x, mean, var):
-        coefficient = 1 / np.sqrt(2 * np.pi * var)
-        exponent = np.exp(-(x - mean) ** 2 / (2 * var))
-        return coefficient * exponent
-
-
-# Stacking Classifier
-class StackingClassifierManual:
-    def __init__(self, classifiers, meta_classifier):
-        self.classifiers = classifiers
-        self.meta_classifier = meta_classifier
-
-    def fit(self, X, y):
-        base_preds = np.zeros((X.shape[0], len(self.classifiers)))
-        for i, clf in enumerate(self.classifiers):
-            clf.fit(X, y)
-            base_preds[:, i] = clf.predict(X)
-        self.meta_classifier.fit(base_preds, y)
-
-    def predict(self, X):
-        base_preds = np.zeros((X.shape[0], len(self.classifiers)))
-        for i, clf in enumerate(self.classifiers):
-            base_preds[:, i] = clf.predict(X)
-        return self.meta_classifier.predict(base_preds)
-
-
-# Initialize classifiers
-clf1 = KNeighborsClassifierManual(n_neighbors=3)
-clf2 = DecisionTreeClassifierManual(max_depth=5)
-clf3 = RandomForestClassifierManual(n_estimators=10, max_depth=5)
-clf4 = GaussianNBManual()
-meta_clf = LogisticRegressionManual(learning_rate=0.01, num_iterations=1000)
-
-# Create Stacking Classifier
-stack = StackingClassifierManual(classifiers=[clf1, clf2, clf3, clf4], meta_classifier=meta_clf)
+# defining the logistic regression function
+logreg = LogisticRegressionManual(learning_rate=0.01, num_iterations=1000)
 
 # Train the stacking model
-stack.fit(X_train, y_train)
+logreg.fit(X_train, y_train)
 
 # Make predictions on the test set
-y_pred_class = stack.predict(X_test)
+y_pred_class = logreg.predict(X_test)
 
 # Evaluate the model
 accuracy = evalClassModel(y_test, y_pred_class, y_pred_class)
-print(f'Accuracy of the Stacking Model: {accuracy}')
+print(f'Accuracy of the Model: {accuracy}')
 
 # Save the model to a file
 with open("manual_model.pkl", "wb") as file:
-    pickle.dump(stack, file)
+    pickle.dump(logreg, file)
 
 print("Model trained and saved as manual_model.pkl")
